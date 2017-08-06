4b50835ef72d7ac5d0d8af1347f17949e9fabc41
Benigno Calvo <benignoc@gmail.com>
2017-08-06 10:44:12 +0200

    Crop by marking which pages to delete

--- pdfcrop.py
+++ pdfcrop.py
@@ -1,27 +1,87 @@
 #!/usr/bin/env python3
 
+"""
+   program name: pdfcrop
+   description: Script that allows to crop a PDF removing or keeping pages
+   created by: Benigno Calvo
+   creation date: 05-08-2016
+   Modification History:
+   06-08-2016: added ability to specify pages to delete instead of pages to keep.
+"""
+
+import sys
+# from string import split
 from optparse import OptionParser
+from PyPDF2 import PdfFileReader, PdfFileWriter
 
 def parse_input():
-    usage = "Usage: %prog [options]"
+    usage = "usage: %prog [options]"
     parser = OptionParser(usage=usage)
 
-    parser.add_option("-f","--file",dest="filename", help = "Input pdf filename and path",
-    metavar = "FILE")
+    parser.add_option("-f","--file",
+                      dest="filename",
+                      help="input pdf filename and path",
+                      default="CVen Benigno Calvo 2016.pdf",
+                      metavar="file")
 
-    parser.add_option("-k","--keep",dest="keep", help = "Input list of pages to keep, in format [1,3,42]",
-    type= "string")
+    parser.add_option("-k", "--keep",
+                      dest="keep",
+                      help="input list of pages to keep, separated by commas 1,3,42",
+                    #   default="1,2",
+                      type="string")
 
-    parser.add_option("-o","--output",dest="output",help="Output pdf filename including path",
-    default = "cropped.pdf")
+    parser.add_option("-d", "--delete",
+                      dest="delete",
+                      help="input list of pages to delete, separated by commas 1,3,42",
+                      default="3,4",
+                      type="string")
+
+    parser.add_option("-o", "--output",
+                      dest="output",
+                      help="output pdf filename including path",
+                      default="cropped.pdf")
 
     (options, args) = parser.parse_args()
+    # if options.filename is None:
+    #     options.filename = input('enter input pdf filename:')
+
+    # if options.keep is None:
+    #     options.keep = input('enter page numbers to keep ie. 2,23,42:')
+
     return (options, args)
-    
+
+def parse_pages(str_pages):
+    """takes a string from the options input and turns
+    it into an array for further use"""
+    tmp_pages = str_pages.split(",")
+    pages = list()
+    for i in tmp_pages:
+        pages.append(int(i)-1)
+    return pages
 
 def main():
     (options, args) = parse_input()
-    print(options, args)
+    pages_to_keep = list()
+    pages_to_delete = list()
+    if options.keep:
+        pages_to_keep = parse_pages(options.keep)
+    if options.delete:
+        pages_to_delete = parse_pages(options.delete)
+    output = PdfFileWriter()
+    infile = PdfFileReader(options.filename)
+    for i in range(infile.getNumPages()):
+        added = False
+        if i in pages_to_keep:
+            page = infile.getPage(i)
+            added = True
+        if i not in pages_to_delete and not added:
+            page = infile.getPage(i)
+            added = True
+        if added:
+            output.addPage(page)
+    with open(options.output, 'wb') as pdffile:
+        output.write(pdffile)
+        print("Created cropped file:", options.output)
 
 if __name__ == '__main__':
     main()
